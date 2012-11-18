e79c3140609733de3750ae5c33ab9c3c8cdf4db0
Mike Minella <mminella@yahoo.com>
2012-11-18 00:09:15 -0500

    Changed bullet sprite

--- src/entities/bullet.hx
+++ src/entities/bullet.hx
@@ -11,17 +11,24 @@ class Bullet extends Entity
 	
 	public function new(x:Float, y:Float)    
 	{   
-		super(x, y - 5); 
-		orb = new Spritemap("gfx/small_orb.png");
+		super(x, y - 11); 
+		orb = new Spritemap("gfx/sprite_orb2.png", 21, 21);
+		orb.add( "shoot", [0, 1, 2, 3], 10, true );
+		
 		graphic = orb;
         type = "bullet";
 		exists = true;
 	}
     
+	private function setAnimations()
+    {
+		orb.play("shoot");
+    }
 	
 	public override function update()    
 	{   
-		moveBy(2, 0, "enemy");
+		moveBy(1, 0, "enemy");
+		setAnimations();
 		super.update();
 			
 		if ( collide( CollisionType.STATIC_SOLID , x , y ) != null )
