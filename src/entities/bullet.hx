eb78ff51f4a102dc4c17b782b83cbfb069e9a77e
Mike Minella <mminella@yahoo.com>
2012-11-17 23:17:24 -0500

    Added gun orb and new images

--- src/entities/bullet.hx
+++ src/entities/bullet.hx
@@ -1,17 +1,19 @@
 package entities; 
 
 import com.haxepunk.Entity; 
-import com.haxepunk.graphics.Image; 
+import com.haxepunk.graphics.Image;
+import com.haxepunk.graphics.Spritemap;
 
 class Bullet extends Entity
 {    
 	public static var exists:Bool = false;
+	private var orb:Spritemap;
 	
 	public function new(x:Float, y:Float)    
-	{        
-		super(x, y);         
-		graphic = Image.createCircle(6, 660066 );
-        setHitbox(6, 6);
+	{   
+		super(x, y - 5); 
+		orb = new Spritemap("gfx/small_orb.png");
+		graphic = orb;
         type = "bullet";
 		exists = true;
 	}
@@ -19,7 +21,7 @@ class Bullet extends Entity
 	
 	public override function update()    
 	{   
-		moveBy(7, 0, "enemy");
+		moveBy(2, 0, "enemy");
 		super.update();
 			
 		if ( collide( CollisionType.STATIC_SOLID , x , y ) != null )
